d4fa4139535904330028b905ed8924f8adf2b3ee
石原里穂 <crokota.jmry.23@gmail.com>
2023-08-25 22:52:09 +0900

    add: ルーティング機能の追加

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,26 +1,47 @@
 import React from 'react';
 import logo from './logo.svg';
+import { BrowserRouter, Router, Route, Routes, Link } from 'react-router-dom';
 import './App.css';
+import Home from './components/home';
+import Practice from './components/practice';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/practice">Practice</Link></li>
+        </ul>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<Practice />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
+
+
+{/*function App() {
+  return (
+    <Router>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+        </ul>
+      </nav>
+
+    <Routes>
+      <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes> 
+    </Router>
+  );
+}
+
+*/}
 
 export default App;
