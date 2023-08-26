d0cf3eeab853b42e3ff6e15378ecc08fe20e24b4
石原里穂 <crokota.jmry.23@gmail.com>
2023-08-26 15:07:08 +0900

    add: add button

--- Frontend/src/pages/home.tsx
+++ Frontend/src/pages/home.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import ButtonComponent from '../components/Button';
+import { Link } from 'react-router-dom';
+
 
 function Home() {
-  return <div>ホーム</div>;
+  return (
+    <>
+      <h1>ユースピカ（仮）</h1>
+      <Link to="/create"><ButtonComponent text={"原稿を作成する"} /></Link>
+    </>
+    );
 }
 
 export default Home;
