7da9d457a471d29f10286bf63ccfd60b16e1e2cf
石原里穂 <crokota.jmry.23@gmail.com>
2023-08-26 15:07:08 +0900

    add: add button

--- Frontend/src/pages/home.tsx
+++ Frontend/src/pages/home.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ButtonComponent from "../components/Button";
 import { Link } from "react-router-dom";
-import { Text, Space } from "@mantine/core";
+import { Space } from "@mantine/core";
 
 function Home() {
   return (
