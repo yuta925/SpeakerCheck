3512e4609bdac07d288320a46ef8896b012d0a15
maple-fm <fuma.deguchi@gmail.com>
2023-08-27 05:24:04 +0900

    update: プロンプトの修正

--- Frontend/src/model/hooks.ts
+++ Frontend/src/model/hooks.ts
@@ -9,6 +9,8 @@ type Hooks = {
 };
 
 const APIKEY = process.env.REACT_APP_OPENAI_API_KEY;
+const PROMPT =
+  "今からいう言葉に関して、以下の評価項目に関してフィードバックをしてください.フィードバック評価項目 1. イントネーション (25点満点)・・・イントネーションの変化や調子はどうでしたか？25点中何点ですか? 2. 発音や言葉のはっきりさ、滑舌 (25点満点)・・・発音や言葉のはっきりさ、滑舌の良さについてどう思いますか？25点中何点ですか？ 3. 話すスピーチ (25点満点)・・・話し方の速さやペース、一貫性について評価してください。25点中何点ですか？ 4. 声の大きさ (25点満点)・・・声の大きさや音量は適切でしたか？25点中何点ですか? 合計得点: （合計得点を100点満点で計算） 全体コメント: （全体的な印象や改善点についてコメントしてください） このプロンプトを使用して、各評価項目について25点満点で評価し、最後に全体コメントを提供してください。出力方法としては<評価項目：点数>(コメント)のように出力してください";
 
 export const useHooks = (): Hooks => {
   const mediaRecorder = useRef<MediaRecorder | null>(null);
@@ -29,7 +31,7 @@ export const useHooks = (): Hooks => {
 
   const startRecording = async () => {
     // 録音開始
-
+    setTranscript("");
     const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
     mediaRecorder.current = new MediaRecorder(stream);
     mediaRecorder.current.start();
@@ -43,6 +45,7 @@ export const useHooks = (): Hooks => {
 
   const stopRecording = () => {
     // 録音停止
+    setResponse("");
     mediaRecorder.current?.stop();
     setIsAudio(false);
 
@@ -91,13 +94,11 @@ export const useHooks = (): Hooks => {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          max_tokens: 100,
           model: "gpt-3.5-turbo",
           messages: [
             {
               role: "user",
-              content:
-                "今からいう言葉に関して、イントネーションや滑舌の良し悪しについてフィードバックをしてください",
+              content: PROMPT,
             },
             {
               role: "user",
