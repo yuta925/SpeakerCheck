af16e4f08bf7835de97d1e3612994a0f1b7159c9
石原里穂 <crokota.jmry.23@gmail.com>
2023-08-26 09:49:42 +0900

    add: create.tsxとselect.tsxの追加

--- Frontend/src/pages/home.tsx
+++ Frontend/src/pages/home.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function Home() {
-  return <div>Welcome to the Home Page</div>;
+  return <div>ホーム</div>;
 }
 
 export default Home;
---
+++ Frontend/src/pages/select.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Select() {
+  return <div>原稿選択ページ</div>;
+}
+
+export default Select;
