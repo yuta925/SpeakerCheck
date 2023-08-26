09931746b1272c375c196b50b0d8d7bced3da9d8
maple-fm <fuma.deguchi@gmail.com>
2023-08-26 11:01:34 +0900

    add: ボタンコンポーネントの作成

---
+++ Frontend/src/components/index.tsx
@@ -0,0 +1,18 @@
+import { Button } from "@mantine/core";
+import { text } from "stream/consumers";
+
+type Props = {
+  text: String;
+};
+
+const ButtonComponent = (props: Props) => {
+  const { text } = props;
+
+  return (
+    <Button w="400px" h="52px">
+      {text}
+    </Button>
+  );
+};
+
+export default ButtonComponent;
