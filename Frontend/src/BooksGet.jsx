c5a295cc94390f088ebb202ee23455a9f3d88742
yuta925 <yutarou041@gmail.com>
2023-08-26 20:53:37 +0900

    firebase データ取得成功

--- Frontend/src/BooksGet.jsx
+++ Frontend/src/BooksGet.jsx
@@ -1,4 +1,4 @@
-import { firebaseConfig, firebase, firestore } from "./firebaseConfig";  
+import { firestore } from "./firebaseConfig";  
 import { useState, useEffect } from "react";
 import { collection, getDocs } from "firebase/firestore";
 
@@ -11,7 +11,6 @@ const BooksGet = () => {
     const docRef = collection(firestore,"Books")
     console.log(docRef)
     getDocs(docRef).then((snapshot) => {
-      console.log("あ")
       let results = [];
       console.log(snapshot.docs)
       snapshot.docs.forEach((doc) => {
@@ -19,19 +18,22 @@ const BooksGet = () => {
         results.push({ id: doc.id, ...doc.data() });
       });
       setBooks(results);
+      console.log(books[0])
     }).catch(() => {
       console.log("Error")
     });
   }, []);
 
+  const BookComponent = books.map((book) => (
+    <div key={book.id}>
+      <p>{book.title}</p>
+      <p>{book.author}</p>
+      <p>{book.category}</p>
+    </div>
+  ))
+
   return books[0] ? (
-    books.map((book) => (
-      <div key={book.id}>
-        <p>{book.title}</p>
-        <p>{book.author}</p>
-        <p>{book.category}</p>
-      </div>
-    ))
+    <div>{BookComponent}</div>
     ) : (
     <p>データがありません</p>
   );
