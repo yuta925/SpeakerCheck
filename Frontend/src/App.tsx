5c56985722be9118a021d5c1a9841bb0994eb24c
石原里穂 <crokota.jmry.23@gmail.com>
2023-08-26 09:49:42 +0900

    add: create.tsxとselect.tsxの追加

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import logo from './logo.svg';
 import { BrowserRouter, Router, Route, Routes, Link } from 'react-router-dom';
 import './App.css';
-import Home from './components/home';
-import Practice from './components/practice';
+import Home from './pages/home';
+import Practice from './pages/practice';
+import Create from './pages/create';
+import Select from './pages/select';
 
 function App() {
   return (
@@ -12,11 +14,15 @@ function App() {
         <ul>
           <li><Link to="/">Home</Link></li>
           <li><Link to="/practice">Practice</Link></li>
+          <li><Link to="/create">Create</Link></li>
+          <li><Link to="/select">Select</Link></li>
         </ul>
 
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<Practice />} />
+          <Route path="/practice" element={<Practice />} />
+          <Route path="/create" element={<Create />} />
+          <Route path="/select" element={<Select />} />
         </Routes>
       </div>
     </BrowserRouter>
