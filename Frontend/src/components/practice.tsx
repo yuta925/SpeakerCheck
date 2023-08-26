9e4975d7f45edb35671e7322acf3f76858df7d1d
石原里穂 <crokota.jmry.23@gmail.com>
2023-08-26 09:49:42 +0900

    add: create.tsxとselect.tsxの追加

--- Frontend/src/components/practice.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function About() {
-  return <div>Welcome to the Practice Page</div>;
-}
-
-export default About;
---
+++ Frontend/src/pages/create.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Create() {
+  return <div>原稿作成ページ</div>;
+}
+
+export default Create;
