3e3607237b02b4afd33071769273a283f4964769
maple-fm <fuma.deguchi@gmail.com>
2023-08-27 15:16:34 +0900

    add: ボタンの配置とインプットフィールドの作成

--- Frontend/src/pages/create.tsx
+++ Frontend/src/pages/create.tsx
@@ -1,15 +1,25 @@
 import React from "react";
 import Button from "../components/Button";
-import { Space } from "@mantine/core";
+import { Flex, TextInput, Container } from "@mantine/core";
 
 const Create = () => {
+  const [input, setInput] = React.useState("");
+
   return (
     <div>
-      <div>
+      <Flex justify="flex-end" gap="md" mx={63}>
         <Button text={"下書き保存"} width={"base"}></Button>
-        <Space w="md"></Space>
         <Button text={"登録"} width={"base"}></Button>
-      </div>
+      </Flex>
+      <TextInput
+        value={input}
+        placeholder="題名"
+        h={54}
+        mx={63}
+        size="lg"
+        py={32}
+        onChange={(event) => setInput(event.currentTarget.value)}
+      />
     </div>
   );
 };
