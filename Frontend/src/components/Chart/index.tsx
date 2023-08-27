8510ae66e99a981fc19e2df582378113c6721dc6
石原里穂 <crokota.jmry.23@gmail.com>
2023-08-27 16:35:49 +0900

    update: チャートファイル更新2

--- Frontend/src/components/Chart/index.tsx
+++ Frontend/src/components/Chart/index.tsx
@@ -1,6 +1,6 @@
 import React, { PureComponent } from 'react';
 import { text } from "stream/consumers";
-import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, Label } from 'recharts';
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, Label, ReferenceArea } from 'recharts';
 import { Center } from '@mantine/core';
 
 
@@ -8,6 +8,14 @@ type Props = {
   name: String;
   score: Number;
 };
+
+const style={
+  fontSize: "25px"
+}
+
+const formatter= (score: Number)=>{
+  return `${score}/25`;
+}
 
 const ChartComponent = (props: Props) => {
   const { name, score } = props;
@@ -19,17 +27,15 @@ const data = [
 ]
 
   return (
-    <Center>
+ <Center>
         <BarChart
         width={600}
-        height={100}
+        height={75}
         layout="vertical"
         data={data}>
-        <XAxis type="number" domain={[0, 25]} hide={true}/>
+        <XAxis type="number" domain={[0, 25]} hide={true} />
         <YAxis dataKey="name" type="category" fontSize={"30px"} tickLine={false} axisLine={false} width={200} />
-        <Bar dataKey="score" fill="#8884d8" background={{ fill: '#eee' }}>
-          <Label position="right"></Label>
-
+        <Bar dataKey="score" fill="#9ACDFF" background={{ fill: '#eee'}} label={{position: "right", style, formatter}} >
         </Bar>
 </BarChart>
 </Center>
