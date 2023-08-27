c769a0a5a419157b3b23dd295647c324f9451ca2
maple-fm <fuma.deguchi@gmail.com>
2023-08-27 18:25:53 +0900

    delete: 不要なものたちの削除

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -4,7 +4,6 @@ import Home from "./pages/home";
 import Practice from "./pages/practice";
 import Create from "./pages/create";
 import Select from "./pages/select";
-import CharComponent from "./components/Panel";
 
 function App() {
   return (
