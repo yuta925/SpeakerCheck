5392141972608844bf69d65ed8723ba13bb45ca4
maple-fm <fuma.deguchi@gmail.com>
2023-08-27 20:42:37 +0900

    delete: 不要なタグの削除

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,19 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import { BrowserRouter, Router, Route, Routes, Link } from 'react-router-dom';
-import './App.css';
-import Home from './pages/home';
-import Practice from './pages/practice';
-import Create from './pages/create';
-import Select from './pages/select';
+import React from "react";
+import logo from "./logo.svg";
+import { BrowserRouter, Router, Route, Routes, Link } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/home";
+import Practice from "./pages/practice";
+import Create from "./pages/create";
+import Select from "./pages/select";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        
-      <p><Link to="/">Home</Link></p>
-      
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/practice" element={<Practice />} />
