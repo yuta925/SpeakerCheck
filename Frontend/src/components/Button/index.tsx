78873fc3709f5c45458bbee7947b4a9506c3b873
maple-fm <fuma.deguchi@gmail.com>
2023-08-27 22:12:54 +0900

    update: コールバック関数の追加

--- Frontend/src/components/Button/index.tsx
+++ Frontend/src/components/Button/index.tsx
@@ -3,14 +3,15 @@ import { Button } from "@mantine/core";
 type Props = {
   text: String;
   width: String;
+  onClick?: () => void;
 };
 
 const ButtonComponent = (props: Props) => {
-  const { text, width } = props;
+  const { text, width, onClick } = props;
   const size = width === "base" ? 164 : 400;
 
   return (
-    <Button w={size} h="52px" color="dark">
+    <Button w={size} h="52px" color="dark" onClick={onClick}>
       {text}
     </Button>
   );
