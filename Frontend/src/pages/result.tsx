5a55d985e747ba61990e6b3390e309811af4e6dc
石原里穂 <crokota.jmry.23@gmail.com>
2023-08-28 00:33:42 +0900

    add: コンポーネントインポート

--- Frontend/src/pages/result.tsx
+++ Frontend/src/pages/result.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import { Text } from "@mantine/core";
 import ScoreComponent from "../components/Score"
+import ChartList from '../components/Chart/List';
+import CharComponent from '../components/Panel';
+import ButtonComponent from '../components/Button';
 
 const Result = () => {
     return (
-        <div>
+        <>
             <Text>採点結果ページ</Text>
             <ScoreComponent text='100'></ScoreComponent>
-        </div>
+            <ChartList data={[10, 10, 25, 20]}></ChartList>
+            <CharComponent text={"すてき"}/>
+            <ButtonComponent text={"タイトル"} width={"lg"} />
+            <ButtonComponent text={"再チャレンジ"} width={"lg"} />
+        </>
     );
 };
 
