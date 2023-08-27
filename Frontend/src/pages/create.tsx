d0381b20d4bbd31bca27d598c540f06fc3a2f489
maple-fm <fuma.deguchi@gmail.com>
2023-08-27 20:50:40 +0900

    feat: テキストエリアの実装

--- Frontend/src/pages/create.tsx
+++ Frontend/src/pages/create.tsx
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { ChangeEvent, useCallback, useState } from "react";
 import Button from "../components/Button";
-import { Flex, TextInput, Container } from "@mantine/core";
+import { Flex, TextInput, Textarea } from "@mantine/core";
 
 const Create = () => {
   const [input, setInput] = React.useState("");
+  const [text, setText] = useState("");
+  const handleTextChange = useCallback(
+    (e: ChangeEvent<HTMLTextAreaElement>) => {
+      setText(e.target.value);
+      setTimeout(() => {
+        console.log("何かしらデータを保存する処理");
+      }, 1000);
+    },
+    []
+  );
 
   return (
     <div>
-      <Flex justify="flex-end" gap="md" mx={63}>
+      <Flex justify="flex-end" gap="md" mx={63} mt={40}>
         <Button text={"下書き保存"} width={"base"}></Button>
         <Button text={"登録"} width={"base"}></Button>
       </Flex>
@@ -20,6 +30,18 @@ const Create = () => {
         py={32}
         onChange={(event) => setInput(event.currentTarget.value)}
       />
+      <div>
+        <Textarea
+          minRows={23}
+          mx={63}
+          placeholder="原稿"
+          description={text.length + "字"}
+          onChange={handleTextChange}
+          styles={{
+            description: { textAlign: "right" },
+          }}
+        />
+      </div>
     </div>
   );
 };
