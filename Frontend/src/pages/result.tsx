6ceaba197b43b9af60eec83c8aa6f68da55bbb70
石原里穂 <crokota.jmry.23@gmail.com>
2023-08-28 01:13:57 +0900

    update: スタイルの変更

--- Frontend/src/pages/result.tsx
+++ Frontend/src/pages/result.tsx
@@ -1,19 +1,45 @@
 import React from 'react';
-import { Text } from "@mantine/core";
+import { Text, Space } from "@mantine/core";
 import ScoreComponent from "../components/Score"
 import ChartList from '../components/Chart/List';
 import CharComponent from '../components/Panel';
 import ButtonComponent from '../components/Button';
+import { Flex } from "@mantine/core";
+import { Link } from "react-router-dom";
+
+
 
 const Result = () => {
     return (
         <>
             <Text>採点結果ページ</Text>
-            <ScoreComponent text='100'></ScoreComponent>
-            <ChartList data={[10, 10, 25, 20]}></ChartList>
+            <Flex
+            mih={50}
+            gap="lg"
+            justify="center"
+            align="center"
+            direction="Row"
+            wrap="wrap">
+                <ScoreComponent text='100'></ScoreComponent>
+                <ChartList data={[10, 10, 25, 20]}></ChartList>
+            </Flex>
             <CharComponent text={"すてき"}/>
-            <ButtonComponent text={"タイトル"} width={"lg"} />
-            <ButtonComponent text={"再チャレンジ"} width={"lg"} />
+            <Space h="lg" />
+            <Flex
+            mih={50}
+            gap="lg"
+            justify="center"
+            align="center"
+            wrap="wrap"
+            >
+                <Link to="/">
+                <ButtonComponent text={"タイトル"} width={"lg"} />
+                </Link>
+                <Link to="/practice">
+                <ButtonComponent text={"再チャレンジ"} width={"lg"} />
+                </Link>
+            </Flex>
+            <Space h="lg" />
         </>
     );
 };
