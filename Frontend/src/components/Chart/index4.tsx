331a4ef5803accc6a88c0525334436131d793a7b
石原里穂 <crokota.jmry.23@gmail.com>
2023-08-27 20:20:42 +0900

    update: チャート四つのコンポーネント作成

--- Frontend/src/components/Chart/index4.tsx
+++ Frontend/src/components/Chart/index4.tsx
@@ -1,7 +1,27 @@
-import {ChartComponent} from '../components/Chart';
+import ChartComponent from ".";
+import { Box, Center, Flex, Space } from '@mantine/core';
 
-function ChartComponent4(){
-    <ChartComponent />
+type ChartData={
+    text: string;
+    score: number;
+}
+
+type Props = {
+    data: ChartData[];
+  }
+    
+  
+const ChartComponent4 = (props: Props) => {
+    const { data } = props;
+
+    return (
+    <Flex direction={"column"} align={"center"}>
+        {
+            data.map((d)=>(<ChartComponent key={d.text} name={ d.text } score={d.score} />
+            ))
+        }
+    </Flex>
+    )
 }
 
 export default ChartComponent4;
