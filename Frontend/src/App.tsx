a243daef288abdadbbbe57e8be3b61d7a6e8279e
maple-fm <fuma.deguchi@gmail.com>
2023-08-27 19:17:00 +0900

    chore: rechartsのインストール

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,25 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import { BrowserRouter, Router, Route, Routes, Link } from 'react-router-dom';
-import './App.css';
-import Home from './pages/home';
-import Practice from './pages/practice';
-import Create from './pages/create';
-import Select from './pages/select';
-import CharComponent from './components/Panel';
+import React from "react";
+import logo from "./logo.svg";
+import { BrowserRouter, Router, Route, Routes, Link } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/home";
+import Practice from "./pages/practice";
+import Create from "./pages/create";
+import Select from "./pages/select";
+import CharComponent from "./components/Panel";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/practice">Practice</Link></li>
-            <li><Link to="/create">Create</Link></li>
-            <li><Link to="/select">Select</Link></li>
-          </ul>
-      
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/practice" element={<Practice />} />
